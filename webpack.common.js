33dc5241aa3a45b0e3019ad22c12faa57ea08320
MDJ-Mikkel-Djurhuus <mikkel.djurhuus@gmail.com>
2017-11-27 15:03:54 +0100

    fix

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,9 @@ var CleanWebPackPlugin = require("clean-webpack-plugin");
 module.exports = {
     entry:  "./src/index.js",
     output: {
-        filename: "./lib/bingo-generator.js"
+        filename: "./lib/bingo-generator.js",
+        libraryTarget: "umd",
+        libraray: "bingo-generator"
     },
     plugins: [
         new CleanWebPackPlugin(["lib"])
